921bfc40227bc2d8f8778b6b0f5bf28d52d38f59
aemorozov <mr.aemorozov@gmail.com>
2025-08-04 22:06:57 +0300

    change stucture

---
+++ setWebhook.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+bot.setWebHook(`https://${process.env.VERCEL_URL}/api/webhook`)
+    .then(() => {
+        console.log('Webhook установлен успешно!');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error('Ошибка при установке webhook:', err);
+        process.exit(1);
+    });
