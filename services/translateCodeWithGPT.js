afa601eb190103650ae2ef55f0ca71d09513c4d6
aemorozov <mr.aemorozov@gmail.com>
2025-08-06 23:29:40 +0300

    try to fix translate

--- services/translateCodeWithGPT.js
+++ services/translateCodeWithGPT.js
@@ -12,9 +12,7 @@ ${JSON.stringify(tickets, null, 2)}
     iata: 'BCN',
     city: 'Barcelona',
     airline: 'FR',
-    airline_name: 'Ryanair',
-    departure: '2025-08-07',
-    return: '2025-08-27',
+    airline_name: 'Ryanair'
   }
 ]
 
