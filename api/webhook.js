06eb99e63f61c700998965a38bf140f47b372c1d
aemorozov <mr.aemorozov@gmail.com>
2025-08-06 23:25:57 +0300

    try to add links 5

--- api/webhook.js
+++ api/webhook.js
@@ -184,7 +184,7 @@ module.exports = async (req, res) => {
 
             // send result
             try {
-                const message = `✈️ *TOP-10 cheapest flights from ${tickets[0].departure}* ✨:\n\n` + tickets.map(t => {
+                const message = `✈️ *TOP-10 cheapest flights from ${translations[0].city}* ✨:\n\n` + translations.map(t => {
                     const match = translations.find(item => item.iata === t.destination && item.airline === t.airline);
                     const city = match?.city || t.destination;
                     const airline = match?.airline_name || t.airline;
