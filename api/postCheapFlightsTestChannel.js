a14c453005c5f98a83ad75c24987e7a75f61baa2
aemorozov <mr.aemorozov@gmail.com>
2025-10-08 23:50:52 +0300

    add active file for posts to the tests channel

---
+++ api/postCheapFlightsTestChannel.js
@@ -0,0 +1,11 @@
+import { postCheapFlights } from "../bots/channelRomania/index";
+
+export default async function handler(req, res) {
+  try {
+    await postCheapFlights();
+    res.status(200).json({ ok: true, message: "Flights posted" });
+  } catch (err) {
+    console.error("Cron error:", err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+}
