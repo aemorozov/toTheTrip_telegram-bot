295595c36c1706ee55a0a3db419edc0288cbd8e3
aemorozov <mr.aemorozov@gmail.com>
2025-11-09 12:09:35 +0200

    update channelRomania

--- channels/channelRomania/index.js
+++ channels/channelRomania/index.js
@@ -141,13 +141,13 @@ async function postCheapFlights() {
   const titles = [
     "✈️ Best flights from",
     "👀 Look! Best deals from",
-    "🔥 WOW, it's really cheap! From",
-    "⏰ Don't miss! Flights from",
-    "💰 Hot prices alert! From",
+    "🔥 WOW, it's really cheap! Flights from",
+    "⏰ Don't miss! Cheap flights from",
+    "💰 Hot prices alert! Cheap flights from",
     "🛫 Fly cheap from",
     "✨ Amazing deals from",
-    "🎯 Grab it now! From",
-    "🏷️ Unbeatable prices from",
+    "🎯 Grab it now! Cheap flights from",
+    "🏷️ Unbeatable prices for flights from",
     "⭐ Exclusive offers from",
   ];
 
@@ -222,4 +222,149 @@ async function postCheapFlights() {
   );
 }
 
-module.exports = { postCheapFlights };
+// функция для постинга ТОП билетов
+//////////////////////////////
+async function postTOPFlights() {
+  let flights = [];
+  const maxOriginAttempts = 5;
+  let originAttempts = 0;
+
+  while (flights.length === 0 && originAttempts < maxOriginAttempts) {
+    originAttempts++;
+    const origins = getRandomOrigins(3);
+
+    for (const origin of origins) {
+      try {
+        const { data } = await axios.get(
+          "https://api.travelpayouts.com/aviasales/v3/prices_for_dates",
+          {
+            params: {
+              currency: "eur",
+              origin,
+              unique: true,
+              sorting: "price",
+              direct: false,
+              one_way: false,
+              limit: 6,
+              token: TRAVELPAYOUTS_TOKEN,
+            },
+          }
+        );
+
+        const allFlights = data?.data || [];
+        const filteredFlights = allFlights.filter(
+          (f) => f.transfers <= 2 && f.return_transfers <= 2
+        );
+
+        if (!filteredFlights.length) continue;
+
+        if (filteredFlights.length) {
+          flights.push(...filteredFlights);
+          break;
+        }
+      } catch (err) {
+        console.warn(`Ошибка при запросе рейсов из ${origin}:`, err.message);
+        continue;
+      }
+    }
+  }
+
+  if (!flights.length) {
+    console.error(
+      "⚠️ Не удалось найти ни одного рейса после нескольких попыток."
+    );
+    return;
+  }
+
+  const originName = await getCityName(flights[0].origin);
+  const destinationName = await getCityName(
+    flights[Math.floor(Math.random() * flights.length)].destination
+  );
+
+  const titles = [
+    "💸 TOP cheapest flights from",
+    "🏆 Best price deals from",
+    "🔥 Hottest flight offers from",
+    "✈️ Top budget-friendly flights from",
+    "📉 Lowest fares right now from",
+    "🛫 Fly smart — best prices from",
+    "💰 Unmissable cheap flights from",
+    "🌍 TOP travel deals from",
+    "🎯 Cheapest destinations from",
+    "⭐ Best of the best deals from",
+  ];
+
+  const title = titles[Math.floor(Math.random() * titles.length)];
+
+  let message = `<b>${title} ${originName.toUpperCase()} from ${
+    flights[0].price
+  }€</b>\n`;
+
+  for (const flight of flights) {
+    const dtDeparture = DateTime.fromISO(flight.departure_at, {
+      setZone: true,
+    });
+    const dtReturn = DateTime.fromISO(flight.return_at, { setZone: true });
+
+    const short = extractShortLink();
+    const searchPath = `${flight.origin}${dtDeparture.toFormat("ddMM")}${
+      flight.destination
+    }${dtReturn.toFormat("ddMM")}1`;
+    const baseUrl = `https://www.aviasales.com/search/${searchPath}?currency=EUR`;
+    const encodedUrl = encodeURIComponent(baseUrl);
+    const link = `https://tp.media/r?marker=59890&trs=443711&p=4114&u=${encodedUrl}&campaign_id=100`;
+
+    const destinationName = await getCityName(flight.destination);
+
+    message += `
+💸 to <b>${destinationName}</b> about <b>${flight.price}€</b>
+🛫 <b>${dtDeparture.toFormat("dd.MM.yyyy")}</b>  🕐 <b>${dtDeparture.toFormat(
+      "HH:mm"
+    )}</b>
+🛬 <b>${dtReturn.toFormat("dd.MM.yyyy")}</b>  🕐 <b>${dtReturn.toFormat(
+      "HH:mm"
+    )}</b>
+🔗 Link: <a href="${link}"><b>https://${short}</b></a>\n`;
+  }
+
+  message += `\n📢 Share with friends!\n\n🤖 <b>Try our bot: <a href="https://t.me/CheapFlightsToTheTripBot">Cheap Flights Bot</a></b>`;
+
+  // === получаем квадратное изображение
+  const imageBuffer = await getCityImage(destinationName);
+
+  if (!imageBuffer) {
+    console.warn("⚠️ Не удалось получить изображение. Отправляю без фото.");
+    await axios.post(
+      `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+      {
+        chat_id: CHANNEL_ID,
+        text: message,
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+      }
+    );
+    return;
+  }
+
+  console.log("message length:", message.length);
+  const form = new FormData();
+  form.append("chat_id", CHANNEL_ID);
+  form.append("caption", message);
+  form.append("parse_mode", "HTML");
+  form.append("disable_web_page_preview", "true");
+  form.append("photo", imageBuffer, `${destinationName}.jpg`);
+
+  await axios.post(
+    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPhoto`,
+    form,
+    {
+      headers: form.getHeaders(),
+    }
+  );
+
+  console.log(
+    `✅ Flights from ${originName} to ${destinationName} posted to Telegram`
+  );
+}
+
+module.exports = { postCheapFlights, postTOPFlights };
--- channels/encodeLink.js
+++ channels/encodeLink.js
@@ -1,9 +1,10 @@
 function extractShortLink(link) {
   const base = "avia.se/";
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm";
+  const letters =
+    "ABCDq0weEFG1yuiHIdfg3JKLsh2jM4Nkl6zO5PQpax6cRSTU7ovb8VWX9YZrtnm";
   let randomPart = "";
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 4; i++) {
     randomPart += letters.charAt(Math.floor(Math.random() * letters.length));
   }
 
