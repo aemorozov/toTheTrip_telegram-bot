57d945644b194fb69edb2dcdccd621ebb6f4bed7
aemorozov <mr.aemorozov@gmail.com>
2025-11-30 14:46:03 +0200

    add api Spain

---
+++ api/postTopForTodaySpain.js
@@ -0,0 +1,11 @@
+import { TopForTodaySpain } from "../channels/channelSpain/index";
+
+export default async function handler(req, res) {
+  try {
+    await TopForTodaySpain();
+    res.status(200).json({ ok: true, message: "Flights posted" });
+  } catch (err) {
+    console.error("Cron error:", err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+}
