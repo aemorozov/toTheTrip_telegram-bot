62de2cce23515ba0a050b6e971f3297b0093310f
aemorozov <mr.aemorozov@gmail.com>
2025-12-06 21:25:19 +0200

    update rateFlight for France

--- channels/channelFrance/index.js
+++ channels/channelFrance/index.js
@@ -46,25 +46,25 @@ function rateFlight(f) {
   const transfers = Math.max(f.transfers, f.return_transfers);
 
   // === 1. Супер дешёвые и без пресадок — всегда да
-  if (price < 70) {
-    f.transfers === 0
-      ? console.log(
-          "Rated: ",
-          f.originName,
-          f.destinationName,
-          dist,
-          price,
-          "Transfers: ",
-          f.transfers,
-          f.return_transfers
-        )
-      : "";
-    return f.transfers === 0;
-  }
+  // if (price < 70) {
+  //   f.transfers === 0
+  //     ? console.log(
+  //         "Rated: ",
+  //         f.originName,
+  //         f.destinationName,
+  //         dist,
+  //         price,
+  //         "Transfers: ",
+  //         f.transfers,
+  //         f.return_transfers
+  //       )
+  //     : "";
+  //   return f.transfers === 0;
+  // }
 
   // === 2. До 2000 км — принимаем ТОЛЬКО прямые
   if (dist < 2000) {
-    transfers === 0 && price <= 100
+    transfers === 0 && price <= 70
       ? console.log(
           "Rated: ",
           f.originName,
@@ -76,12 +76,12 @@ function rateFlight(f) {
           f.return_transfers
         )
       : "";
-    return transfers === 0 && price <= 100;
+    return transfers === 0 && price <= 70;
   }
 
   // === 2. До 3500 км — принимаем ТОЛЬКО прямые
   if (dist < 3500) {
-    transfers === 0 && price <= 150
+    transfers === 0 && price <= 130
       ? console.log(
           "Rated: ",
           f.originName,
@@ -93,12 +93,12 @@ function rateFlight(f) {
           f.return_transfers
         )
       : "";
-    return transfers === 0 && price <= 150;
+    return transfers === 0 && price <= 130;
   }
 
   // === 3. 3500–5000 км — 1 пересадка допускается, но должны быть причины:
   if (dist < 5000) {
-    transfers === 0 && price <= 250
+    transfers === 0 && price <= 200
       ? console.log(
           "Rated: ",
           f.originName,
@@ -110,7 +110,7 @@ function rateFlight(f) {
           f.return_transfers
         )
       : "";
-    transfers === 1 && price <= 200
+    transfers === 1 && price <= 150
       ? console.log(
           "Rated: ",
           f.originName,
@@ -122,15 +122,15 @@ function rateFlight(f) {
           f.return_transfers
         )
       : "";
-    if (transfers === 0 && price <= 250) return true;
-    if (transfers === 1 && price <= 200) return true; // пересадка только если дешёвый
+    if (transfers === 0 && price <= 200) return true;
+    if (transfers === 1 && price <= 150) return true; // пересадка только если дешёвый
     return false;
   }
 
   // === 4. От 5000 км и выше — пересадки нормальны
   // но цена должна соответствовать дальности
   if (dist >= 5000) {
-    transfers <= 1 && price <= 450
+    transfers <= 1 && price <= 400
       ? console.log(
           "Rated: ",
           f.originName,
@@ -142,7 +142,7 @@ function rateFlight(f) {
           f.return_transfers
         )
       : "";
-    if (transfers <= 1 && price <= 450) return true;
+    if (transfers <= 1 && price <= 400) return true;
     return false;
   }
 
